51d77980ef19761214da5e1e619fe26357a22386
Michael <michael.straub@denner.ch>
2020-12-29 04:34:18 +0100

    🚀 [FEATURE] Added dotenv for testing

--- packages/testing/src/main.ts
+++ packages/testing/src/main.ts
@@ -1,13 +1,23 @@
 import { WatsonApplicationFactory } from '@watson/core';
+import { config } from 'dotenv';
 
 import { ApplicationModule } from './modules/app.module';
 
 const bootstrap = async () => {
-  const app = await WatsonApplicationFactory.create(ApplicationModule, {
-    discordAuthToken: "Asdjads",
-  });
+  const {
+    parsed: { DISCORD_TOKEN: token },
+  } = config({
+    path: __dirname + "/../.env",
+  }) as any;
 
+  const app = await WatsonApplicationFactory.create(ApplicationModule, {
+    discordAuthToken: token,
+  });
   app.addGlobalPrefix("!");
+  app.setActivity({
+    name: "doing stuff",
+    type: "PLAYING",
+  });
 
   app.start().then((res) => console.log("started"));
 };
