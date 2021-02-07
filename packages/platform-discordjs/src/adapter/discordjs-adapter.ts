05edc3b4605897fc7f4a8b7c94a7fc9652c1b997
Michael <michael.straub@denner.ch>
2021-02-07 22:15:13 +0100

    🚀 [FEATURE] Abstracted discord adapter package

---
+++ packages/platform-discordjs/src/adapter/discordjs-adapter.ts
@@ -0,0 +1,39 @@
+import { AbstractDiscordAdapter } from '@watsonjs/core';
+import { ClientEvents } from 'discord.js';
+import { Observable } from 'rxjs';
+
+export class DiscordJsAdapter extends AbstractDiscordAdapter {
+  constructor() {}
+
+  init(): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+
+  initializeSlashCommands(): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+
+  protected setUserActivity(): void {
+    throw new Error("Method not implemented.");
+  }
+
+  protected login(): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+
+  protected destroy(): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+
+  public createListener<E extends keyof ClientEvents>(
+    event: E
+  ): Observable<ClientEvents[E]> {
+    throw new Error("Method not implemented.");
+  }
+
+  public createWSListener<T extends {}, E extends string>(
+    event: E
+  ): Observable<any> {
+    throw new Error("Method not implemented.");
+  }
+}
