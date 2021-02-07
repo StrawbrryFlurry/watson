5216572ec80257a8a6a367e8fa60f381efc53579
Michael <michael.straub@denner.ch>
2021-02-07 22:16:01 +0100

    ✏ [UPDATE] Changed to match docs

--- packages/example/src/modules/exceptions/exception.module.ts
+++ packages/example/src/modules/exceptions/exception.module.ts
@@ -3,6 +3,9 @@ import { Module } from '@watsonjs/common';
 import { ExceptionsReceiver } from './exception.receiver';
 
 @Module({
+  imports: [],
   receivers: [ExceptionsReceiver],
+  providers: [],
+  exports: [],
 })
 export class ExceptionsModule {}
