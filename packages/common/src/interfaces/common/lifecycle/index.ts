2330861149cb523b657172c568aa2166a500bc32
Michael <michael.straub@denner.ch>
2021-02-19 23:59:14 +0100

    ✏ [UPDATE] Naming change

--- packages/common/src/interfaces/common/lifecycle/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./application-bootstrap.interface";
-export * from "./application-shutdown.interface";
-export * from "./module-destroy.interface";
-export * from "./module-init.interface";
---
+++ packages/common/src/interfaces/hooks/index.ts
@@ -0,0 +1,4 @@
+export * from "./on-application-bootstrap.hook";
+export * from "./on-application-shutdown.hook";
+export * from "./on-module-destroy.hook";
+export * from "./on-module-init.hook";
