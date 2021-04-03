3211cc2050f4e69aa243f4433d7e1db0f8742448
Michael <michael.straub@denner.ch>
2021-04-03 05:51:12 +0200

    ✏ [UPDATE] Added *provider* type

--- packages/common/src/interfaces/common/index.ts
+++ packages/common/src/interfaces/common/index.ts
@@ -1,4 +1,5 @@
 export * from "./custom-provider.interface";
 export * from "./dynamic-module.interface";
 export * from "./injectable.interface";
+export * from "./provider.interface";
 export * from "./receiver.interface";
