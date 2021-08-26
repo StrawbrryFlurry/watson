705e10c104c46ddb621d1882ab5bd2e261fdf3ee
Michael <michael.straub@denner.ch>
2021-08-26 13:59:26 +0200

    🚀 [FEATURE] Implemented PrefixTrie

--- packages/common/src/data-structures/trie/trie-node.ts
+++ packages/common/src/data-structures/trie/trie-node.ts
@@ -1,6 +1,53 @@
 import { char } from '@interfaces/command';
 
-export class TrieNode {
-  next: TrieNode;
-  value: char | null;
+export const TRIE_ROOT_NODE = Symbol("TrieRootNode");
+
+export class TrieNode extends Map<char, TrieNode> {
+  public isLeaf = false;
+
+  public get word(): string {
+    return this._word;
+  }
+  private _word: string;
+
+  public get parent(): TrieNode | Symbol {
+    return this._parent;
+  }
+  private _parent: TrieNode | Symbol;
+
+  public get parentKey(): char {
+    return this._parentKey;
+  }
+  private _parentKey: char;
+
+  public get isRootNode() {
+    return this._isRootNode;
+  }
+  private _isRootNode: boolean;
+
+  constructor(parent: TrieNode | Symbol, parentKey: char) {
+    super();
+    this._parent = parent;
+    this._parentKey = parentKey;
+
+    if (parent === TRIE_ROOT_NODE) {
+      this._word = parentKey;
+      this._isRootNode = true;
+    } else {
+      this._word = (parent as TrieNode).word + parentKey;
+      this._isRootNode = false;
+    }
+  }
+
+  public insert(char: char) {
+    const existingNextNode = this.get(char);
+
+    if (existingNextNode) {
+      return existingNextNode;
+    }
+
+    const next = new TrieNode(this, char);
+    this.set(char, next);
+    return next;
+  }
 }
--- packages/common/src/data-structures/trie/trie.ts
+++ packages/common/src/data-structures/trie/trie.ts
@@ -1,11 +1,75 @@
-import { TrieNode } from './trie-node';
+import { TRIE_ROOT_NODE, TrieNode } from './trie-node';
 
 export class Trie {
-  private _node: TrieNode;
+  private _node: TrieNode = new TrieNode(TRIE_ROOT_NODE, "");
 
-  public find() {}
-  public contains() {}
+  public contains(word: string): boolean {
+    let node = this._node;
+    word = this.normalizeInput(word);
 
-  public addPrefix() {}
-  public removeUnused() {}
+    for (let i = 0; i < word.length; i++) {
+      let char = word[i];
+      const nextNode = node.get(char);
+
+      if (!nextNode) {
+        return false;
+      }
+
+      if (nextNode.isLeaf) {
+        return true;
+      }
+
+      node = nextNode;
+    }
+
+    return false;
+  }
+
+  public find(word: string, best: boolean = false) {
+    let node = this._node;
+    word = this.normalizeInput(word);
+    let lastLeafMatch: TrieNode = null;
+
+    for (let i = 0; i < word.length; i++) {
+      let char = word[i];
+      const nextNode = node.get(char);
+
+      if (!nextNode) {
+        return lastLeafMatch;
+      }
+
+      if (!nextNode.isLeaf) {
+        node = nextNode;
+        continue;
+      }
+
+      if (!best) {
+        return nextNode;
+      }
+
+      if (nextNode.size === 0) {
+        return nextNode;
+      }
+
+      lastLeafMatch = nextNode;
+      node = nextNode;
+    }
+
+    return null;
+  }
+
+  public addPrefix(prefix: string) {
+    prefix = this.normalizeInput(prefix);
+    let node = this._node;
+
+    for (let i = 0; i < prefix.length; i++) {
+      const char = prefix[i];
+      node = node.insert(char);
+    }
+    node.isLeaf = true;
+  }
+
+  public normalizeInput(str: string): string {
+    return str.trim().toLowerCase();
+  }
 }
