9320836c3fed4ace7dad412889b860481d955d16
Michael <michael.straub@denner.ch>
2021-09-25 06:37:18 +0200

    ✏ [UPDATE] Add new static component fields (WIP)

--- packages/common/src/fields.ts
+++ packages/common/src/fields.ts
@@ -1,4 +1,12 @@
 /**
+ *  TODO: If we're going to use the closure compiler,
+ * we need to make sure that these property names
+ * can still be resolved even if closure is going
+ * to rename them to a more compact / efficient
+ * one.
+ */
+
+/**
  * The element ID is used by the DI
  * framework to determine wether a
  * injector has a specific provider.
@@ -15,5 +23,27 @@
  * - [-1] Context Providers which can
  *   only be resolved by a context
  *   injector
+ * - [-2] The current injector
  */
-export const WATSON_ELEMENT_ID = "__WATSON__ELEMENT__ID__";
+export const WATSON_ELEMENT_ID = "ɵeid";
+
+export enum InjectorElementId {
+  Internal = 0,
+  Context = -1,
+  Injector = -2,
+}
+
+/**
+ * Any type (`Object`) that is resolved by
+ * an injector will have this filed, referencing
+ * the injector that has created it.
+ */
+export const WATSON_INJ_IMPL = "ɵinj";
+
+/**
+ * Any type (`Object`) that is resolved by
+ * an injector will have this field, referencing
+ * the {@link Binding } type from which it was
+ * created.
+ */
+export const WATSON_BINDING_DEF = "ɵbind";
